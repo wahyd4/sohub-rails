69f3b77bfc08ec4cd166ae3fa081c243d7fe93c8
junv <wahyd4@gmail.com>
2013-07-19 22:10:39 +0800

    initial project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SohubRails::Application.config.secret_key_base = '304a2ada3025e0bc98a9ce6fd13d308ab17dbb5536aa6f8df0b628537b3884c1dab13630eafd57cdd03c87f1b34544b0eac6556ce98124ff0a92cae5fe670b97'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SohubRails::Application.config.session_store :cookie_store, key: '_sohub-rails_session'
