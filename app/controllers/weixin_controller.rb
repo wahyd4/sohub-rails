e90c69832651165f60ac3abd9fa977fc94d216b4
junv <wahyd4@gmail.com>
2013-08-05 21:54:07 +0800

    add weixin simple reply

--- app/controllers/weixin_controller.rb
+++ app/controllers/weixin_controller.rb
@@ -13,14 +13,16 @@ class WeixinController < ApplicationController
 
   def query
     message = parse_xml_to_hash(params[:xml])
+    Rails.logger.info "[XML]===="+ params[:xml].to_s
     case message.message_type
       when 'text'
-        reply_text_message message, "Hello Forkers!"
+        result = reply_text_message message, "Hello Forkers!"
       when 'image'
-        reply_text_message message, "Image"
+        result = reply_text_message message, "Image"
       else
-        reply_text_message message, "对不起，你发的消息我不能分辨"
+        result = reply_text_message message, "对不起，你发的消息我不能分辨"
     end
+    render xml: result
   end
 
 end
