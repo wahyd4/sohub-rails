b7e8d1187d389bd3b1a238c0ca3e8292faa7647a
junv <wahyd4@gmail.com>
2013-08-06 09:02:36 +0800

    try to get weixin post data

--- app/controllers/weixin_controller.rb
+++ app/controllers/weixin_controller.rb
@@ -12,22 +12,17 @@ class WeixinController < ApplicationController
   end
 
   def query
-    Rails.logger.info "[1]===="+ request.body.to_s
-    Rails.logger.info "[2]===="+ request.raw_post.to_s
     Rails.logger.info "[3]===="+ request.request_parameters.to_s
-    Rails.logger.info "[XML]===="+ request.parse_query.to_s
-
-    #body = Hash.from_xml(request.body.read)
-    #Rails.logger.info "[XML]===="+ body.to_s
-    #message = parse_xml_to_hash(body['xml'])
-    #case message.message_type
-    #  when 'text'
-    #    result = reply_text_message message, "Hello Forkers!"
-    #  when 'image'
-    #    result = reply_text_message message, "Image"
-    #  else
-    #    result = reply_text_message message, "对不起，你发的消息我不能分辨"
-    #end
+    var weixin_params= request.request_parameters
+    message = parse_xml_to_hash(weixin_params[:xml])
+    case message.message_type
+      when 'text'
+        result = reply_text_message message, "Hello Forkers!"
+      when 'image'
+        result = reply_text_message message, "Image"
+      else
+        result = reply_text_message message, "对不起，你发的消息我不能分辨"
+    end
     render xml: result
   end
 
