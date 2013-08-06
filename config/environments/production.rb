285bc29fc8b7ee172c912922b4ca97e33c46d4f1
junv <wahyd4@gmail.com>
2013-08-06 20:17:12 +0800

    enable s3 on product env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,13 @@ SohubRails::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   config.i18n.available_locales = ['zh-CN', :zh]
+
+  config.paperclip_defaults = {
+      :storage => :s3,
+      :s3_credentials => {
+          :bucket => ENV['AWS_BUCKET'],
+          :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+          :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+      }
+  }
 end
