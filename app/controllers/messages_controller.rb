881a7d9408e98d229e3e11e44405cdde758b01e1
junv <wahyd4@gmail.com>
2013-08-13 23:41:00 +0800

    update message json

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,7 +6,7 @@ class MessagesController < ApplicationController
 
   def show_by_type
     type = params[:type]
-    @messages = Message.where(message_type:type).order('created_at DESC')
-    render json: @messages
+    @messages = Message.where(message_type: type).order('created_at DESC')
+    render json: @messages.to_json(include: {user: {only: [:avatar, :display_name]}})
   end
 end
